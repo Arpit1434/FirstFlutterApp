ba332c2c53177ca211cd2e6a22b6e7722a69ccac
Arpit1434 <arpit.tiwari1434@gmail.com>
2023-08-28 20:05:26 +0530

    deathnote fan page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text("Hello Flutter"),
+    home: Home(),
   ));
 }
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            "DeathNote",
+          style: TextStyle(
+            fontFamily: "IndieFlower",
+            fontSize: 26.0
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image.asset("assets/deathnote.jpg"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("You liked this picture!");
+        },
+        backgroundColor: Colors.grey[200],
+        child: Icon(
+          Icons.favorite,
+          color: Colors.pink,
+          size: 24.0,
+        ),
+      ),
+    );
+  }
+}
+
 
